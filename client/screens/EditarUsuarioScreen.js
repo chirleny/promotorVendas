2de8daeb6e000cd9ad02708763c8ee7f5be03eba
Nascimento <chirleny.nascimento@accenture.com>
2023-05-07 13:04:00 -0300

    Deleção de arquivos

--- client/screens/EditarUsuarioScreen.js
+++ client/screens/EditarUsuarioScreen.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react'; 
 import { View, Text, Modal, TextInput, StyleSheet, TouchableOpacity } from 'react-native';
 import Axios from 'axios';
 import {
